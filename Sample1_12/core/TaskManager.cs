6ec9c60bfeac5e54532c05538e193e5f58d374f9
Shuhei Nomura <info@danmaq.com>
2011-04-14 14:14:49 +0000

    サンプル1_12のベースを作成。

---
+++ Sample1_12/core/TaskManager.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections.Generic;
+
+namespace Sample1_11.core
+{
+
+	/// <summary>
+	/// タスク管理クラス。
+	/// </summary>
+	class TaskManager<T>
+		: ITask
+		where T : ITask
+	{
+
+		/// <summary>タスク一覧。</summary>
+		public readonly List<T> tasks = new List<T>();
+
+		/// <summary>
+		/// コンストラクタ。
+		/// </summary>
+		public TaskManager()
+		{
+		}
+
+		/// <summary>
+		/// タスクを開始します。
+		/// </summary>
+		public void setup()
+		{
+			int length = tasks.Count;
+			for (int i = 0; i < length; i++)
+			{
+				tasks[i].setup();
+			}
+		}
+
+		/// <summary>
+		/// 1フレーム分の更新を行います。
+		/// </summary>
+		public void update()
+		{
+			int length = tasks.Count;
+			for (int i = 0; i < length; i++)
+			{
+				tasks[i].update();
+			}
+		}
+
+		/// <summary>
+		/// 描画します。
+		/// </summary>
+		/// <param name="graphics">グラフィック データ。</param>
+		public void draw(Graphics graphics)
+		{
+			int length = tasks.Count;
+			for (int i = 0; i < length; i++)
+			{
+				tasks[i].draw(graphics);
+			}
+		}
+	}
+}
---
+++ Sample1_12/scene/Title.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Sample1_11.core;
+
+namespace Sample1_11.scene
+{
+
+	/// <summary>
+	/// タイトル画面。
+	/// </summary>
+	class Title
+		: IScene
+	{
+
+		/// <summary>クラス オブジェクト。</summary>
+		public static readonly IScene instance = new Title();
+
+		/// <summary>
+		/// コンストラクタ。
+		/// </summary>
+		private Title()
+		{
+			next = this;
+		}
+
+		/// <summary>次に遷移するシーン。</summary>
+		public IScene next
+		{
+			get;
+			private set;
+		}
+
+		/// <summary>
+		/// ゲーム シーンの初期化を行います。
+		/// </summary>
+		public void setup()
+		{
+			Score.instance.drawNowScore = false;
+		}
+
+		/// <summary>
+		/// 1フレーム分の更新を行います。
+		/// </summary>
+		public void update()
+		{
+			next = this;
+			KeyboardState keyState = KeyStatus.instance.keyboardState;
+			if (keyState.IsKeyDown(Keys.Escape))
+			{
+				next = null;
+			}
+			if (keyState.IsKeyDown(Keys.Space))
+			{
+				// ゲーム開始
+				next = GamePlay.instance;
+			}
+		}
+
+		/// <summary>
+		/// 1フレーム分の描画を行います。
+		/// </summary>
+		/// <param name="graphics">グラフィック データ。</param>
+		public void draw(Graphics graphics)
+		{
+			graphics.spriteBatch.DrawString(
+				graphics.spriteFont, "SAMPLE 1", new Vector2(200, 100),
+				Color.Black, 0f, Vector2.Zero, 5f, SpriteEffects.None, 0f);
+			graphics.spriteBatch.DrawString(graphics.spriteFont,
+				"PUSH SPACE KEY.", new Vector2(340, 400), Color.Black);
+			Score.instance.draw(graphics);
+		}
+	}
+}
