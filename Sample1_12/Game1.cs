807064c29bccdedeebc79f147a544b11c98fe779
Shuhei Nomura <info@danmaq.com>
2011-04-24 03:57:40 +0000

    サンプル1_12(Flyweight)を追加。

---
+++ Sample1_12/Game1.cs
@@ -0,0 +1,76 @@
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Sample1_11.core;
+using Sample1_11.scene;
+
+namespace Sample1_11
+{
+
+	/// <summary>
+	/// This is the main type for your game
+	/// </summary>
+	public class Game1
+		: Game
+	{
+
+		/// <summary>画面矩形情報。</summary>
+		public static readonly Rectangle SCREEN = new Rectangle(0, 0, 800, 600);
+
+		/// <summary>描画周りデータ。</summary>
+		private Graphics graphics;
+
+		/// <summary>シーン管理クラス。</summary>
+		private SceneManager mgrScene = new SceneManager(Title.instance);
+
+		/// <summary>タスク管理クラス。</summary>
+		private readonly TaskManager<ITask> mgrTask = new TaskManager<ITask>();
+
+		/// <summary>
+		/// Constructor.
+		/// </summary>
+		public Game1()
+		{
+			new GraphicsDeviceManager(this);
+			Content.RootDirectory = "Content";
+			mgrTask.tasks.AddRange(new ITask[] { KeyStatus.instance, mgrScene });
+		}
+
+		/// <summary>
+		/// LoadContent will be called once per game and is the place to load
+		/// all of your content.
+		/// </summary>
+		protected override void LoadContent()
+		{
+			graphics = new Graphics(this);
+			mgrTask.setup();
+		}
+
+		/// <summary>
+		/// Allows the game to run logic such as updating the world,
+		/// checking for collisions, gathering input, and playing audio.
+		/// </summary>
+		/// <param name="gameTime">Provides a snapshot of timing values.</param>
+		protected override void Update(GameTime gameTime)
+		{
+			mgrTask.update();
+			if (mgrScene.nowScene == null)
+			{
+				Exit();
+			}
+			base.Update(gameTime);
+		}
+
+		/// <summary>
+		/// This is called when the game should draw itself.
+		/// </summary>
+		/// <param name="gameTime">Provides a snapshot of timing values.</param>
+		protected override void Draw(GameTime gameTime)
+		{
+			GraphicsDevice.Clear(Color.CornflowerBlue);
+			graphics.spriteBatch.Begin();
+			mgrTask.draw(graphics);
+			graphics.spriteBatch.End();
+			base.Draw(gameTime);
+		}
+	}
+}
---
+++ Sample1_12/core/Score.cs
@@ -0,0 +1,98 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Sample1_11.core
+{
+
+	/// <summary>
+	/// スコア情報。
+	/// </summary>
+	class Score
+		: ITask
+	{
+
+		/// <summary>クラス オブジェクト。</summary>
+		public static readonly Score instance = new Score();
+
+		/// <summary>
+		/// コンストラクタ。
+		/// </summary>
+		private Score()
+		{
+		}
+
+		/// <summary>エクステンドの閾値。</summary>
+		private const int EXTEND_THRESHOLD = 500;
+
+		/// <summary>現在のスコアを描画するかどうか。</summary>
+		public bool drawNowScore = false;
+
+		/// <summary>前フレームのスコア。</summary>
+		private int prev;
+
+		/// <summary>現在のスコア。</summary>
+		public int now
+		{
+			get;
+			private set;
+		}
+
+		/// <summary>ハイスコア。</summary>
+		public int highest
+		{
+			get;
+			private set;
+		}
+
+		/// <summary>
+		/// スコアをリセットします。
+		/// </summary>
+		public void setup()
+		{
+			now = 0;
+			prev = 0;
+			drawNowScore = true;
+		}
+
+		/// <summary>
+		/// 1フレーム分の更新を行います。
+		/// </summary>
+		public void update()
+		{
+			// スコアクラスは別段毎フレーム更新するようなものはない。
+		}
+
+		/// <summary>
+		/// スコアを加算します。
+		/// </summary>
+		/// <param name="score">加算されるスコア値。</param>
+		/// <returns>エクステンド該当となる場合、true。</returns>
+		public bool add(int score)
+		{
+			now += score;
+			bool extend = now % EXTEND_THRESHOLD < prev % EXTEND_THRESHOLD;
+			prev = now;
+			if (highest < now)
+			{
+				highest = now;
+			}
+			return extend;
+		}
+
+		/// <summary>
+		/// 描画します。
+		/// </summary>
+		/// <param name="graphics">グラフィック データ。</param>
+		public void draw(Graphics graphics)
+		{
+			if (drawNowScore)
+			{
+				graphics.spriteBatch.DrawString(graphics.spriteFont,
+					"SCORE: " + now.ToString(),
+					new Vector2(300, 560), Color.Black);
+			}
+			graphics.spriteBatch.DrawString(graphics.spriteFont,
+				"HISCORE: " + highest.ToString(), new Vector2(0, 560), Color.Black);
+		}
+	}
+}
